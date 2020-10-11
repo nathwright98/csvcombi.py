ca8bfdb9f854795eda53cddf7c3d552285a9edc5
nathwright98 <n.c.wright@lancaster.ac.uk>
2020-10-11 06:19:13 +0100

    Copyright notice

--- csvcombi.py
+++ csvcombi.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+"""
+csvcombi.py: Combines data in CSV files according to user-defined preferences.
+Copyright(C) 2020 Nathan C. Wright
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
 import csv
 import statistics as stats
 """
